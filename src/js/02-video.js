447c60e41c4f2fb36b5c79693742eb970bd68538
Klimch1k <114351322+Klimch1k@users.noreply.github.com>
2023-03-12 21:52:14 +0200

    task -2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,18 @@
+import Vimeo from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Vimeo(iframe);
+
+function savedTimeVideo(data) {
+  const currentTime = data.seconds;
+  localStorage.setItem('videoplayer-current-time', JSON.stringify(currentTime));
+}
+
+player.on('timeupdate', throttle(savedTimeVideo, 1000));
+
+const savedTime = localStorage.getItem('videoplayer-current-time');
+
+if (savedTime) {
+  player.setCurrentTime(savedTime);
+}
